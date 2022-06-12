be67dd1765fa68580fdfddb81250ef773ce6fcee
poseidon259 <trqhung2591@gmail.com>
2022-06-12 23:32:17 +0700

    final pl

--- script.js
+++ script.js
@@ -5,6 +5,31 @@ $(document).ready(function() {
         } else {
             $('.navbar').removeClass('sticky');
         }
+        if(this.scrollY > 500) {
+            $('.scroll-up-btn').addClass('show');
+        } else {
+            $('.scroll-up-btn').removeClass('show');
+        }
+    });
+
+    // scroll up btn
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({scrollTop: 0});
+    });
+
+    //typing text
+    var typed = new Typed(".typing", {
+        strings: ["Software Engineer", "Web Developer", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    });
+
+    var typed1 = new Typed(".typing-1", {
+        strings: ["Software Engineer", "Web Developer", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
     });
 
     // toggle menu bar
